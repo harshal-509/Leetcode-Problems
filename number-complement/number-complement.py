f423ce9278aa597a8b4dd206af099c64cdca8f2e
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2021-12-27 10:59:31 +0530

    Time: 32 ms (55.58%), Space: 14.2 MB (68.47%) - LeetHub

---
+++ number-complement/number-complement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        ans=""
+        while(num):
+            if(num&1):
+                ans="0"+ans
+            else:
+                ans="1"+ans
+            num=num>>1
+        return int(ans,2)
