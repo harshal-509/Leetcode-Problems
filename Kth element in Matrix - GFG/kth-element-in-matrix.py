8a5793670493844707e891a99d3d831db1d7becb
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-01-25 13:38:03 +0530

    Added solution - LeetHub

---
+++ Kth element in Matrix - GFG/kth-element-in-matrix.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+def kthSmallest(mat, n, k): 
+    # Your code goes here
+    ans=[]
+    for i in range(n):
+        ans+=mat[i]
+    ans.sort()
+    return ans[k-1]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+# Driver Code 
+
+def main():
+    T=int(input())
+    while(T>0):
+        n = int(input())
+        mat=[[0 for j in range(n)] for i in range(n)]
+        line1=[int(x) for x in input().strip().split()]
+        k = int(input())
+
+        temp=0
+        for i in range(n):
+            for j in range (n):
+                mat[i][j]=line1[temp]
+                temp+=1
+        
+        print(kthSmallest(mat, n, k))
+        T-=1
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
+# } Driver Code Ends
