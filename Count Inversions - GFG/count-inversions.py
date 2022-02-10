a4e9229360dcddd37286cbe72bd368a9020513d4
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-02-10 19:43:07 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.py
+++ Count Inversions - GFG/count-inversions.py
@@ -1,40 +1,41 @@
-def ps(arr,temp,l,m,r):
-    i=l
-    j=m
-    k=l
-    ans=0
-    while(i<m and j<=r):
-        if(arr[i]<=arr[j]):
-            temp[k]=arr[i]
-            i+=1
-            k+=1
-        else:
-            temp[k]=arr[j]
-            j+=1
-            k+=1
-            ans+=m-i
-    while(i<m):
-        temp[k]=arr[i]
-        k+=1
-        i+=1
-    while(j<=r):
-        temp[k]=arr[j]
-        k+=1
-        j+=1
-    for i in range(l,r+1):
-        arr[i]=temp[i]
-    return ans
-def hs(arr,temp,l,r):
-    ans=0
-    if(l<r):
-        m=l+(r-l)//2
-        ans+=hs(arr,temp,l,m)
-        ans+=hs(arr,temp,m+1,r)
-        ans+=ps(arr,temp,l,m+1,r)
-    return ans
+
 class Solution:
     def inversionCount(self, arr, n):
         # Your Code Here
+        def ps(arr,temp,l,m,r):
+            i=l
+            j=m
+            k=l
+            ans=0
+            while(i<m and j<=r):
+                if(arr[i]<=arr[j]):
+                    temp[k]=arr[i]
+                    i+=1
+                    k+=1
+                else:
+                    ans+=m-i
+                    temp[k]=arr[j]
+                    j+=1
+                    k+=1
+            while(i<m):
+                temp[k]=arr[i]
+                i+=1
+                k+=1
+            while(j<=r):
+                temp[k]=arr[j]
+                j+=1
+                k+=1
+            for t in range(l,r+1):
+                arr[t]=temp[t]
+            return ans
+        def hs(arr,temp,i,j):
+            ans=0
+            if(i<j):
+                m=i+(j-i)//2
+                ans+=hs(arr,temp,i,m)
+                ans+=hs(arr,temp,m+1,j)
+                ans+=ps(arr,temp,i,m+1,j)
+            return ans
         temp=[0 for i in range(n)]
         return hs(arr,temp,0,n-1)
 #{ 
