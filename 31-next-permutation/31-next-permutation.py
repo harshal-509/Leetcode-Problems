931e9ee05c84811e004fbaacdfaa33e0bf4055a7
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-04-03 14:24:50 +0530

    Time: 58 ms (54.17%), Space: 13.8 MB (79.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -4,24 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        flag=1
+        index=-1
         for i in range(n-2,-1,-1):
             if(nums[i]<nums[i+1]):
-                flag=0
                 index=i
                 break
-        if(flag):
+        if(index==-1):
             nums.sort()
-        else:
-            j=nums[index+1]
-            k=index+1
-            for i in range(index+1,n):
-                if(nums[i]<=j and nums[i]>nums[index]):
-                    k=i
-            nums[index],nums[k]=nums[k],nums[index]
-            i=index+1
-            j=n-1
-            while(i<=j):
-                nums[i],nums[j]=nums[j],nums[i]
-                i+=1
-                j-=1
+            return nums
+        index1=i+1
+        for i in range(index+1,n):
+            if(nums[i]<=nums[index1] and nums[i]>nums[index]):
+                index1=i
+        nums[index1],nums[index]=nums[index],nums[index1]
+        i=index+1
+        j=n-1
+        while(i<j):
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+            j-=1
+        return nums
