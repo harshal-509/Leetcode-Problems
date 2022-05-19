b2442eaad8bb720185f0fc244d2a8f581b1ddcf1
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-05-19 14:06:17 +0530

    Added solution - LeetHub

---
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.py
@@ -0,0 +1,52 @@
+#User function Template for python3
+import heapq
+class MaxHeapObj:
+    def __init__(self,val): self.val = val
+    def __lt__(self,other): return self.val > other.val
+    def __eq__(self,other): return self.val == other.val
+    def __str__(self): return str(self.val)
+class MinHeap(object):
+    def __init__(self): self.h = []
+    def heappush(self, x): heapq.heappush(self.h, x)
+    def heappop(self): return heapq.heappop(self.h)
+    def __getitem__(self, i): return self.h[i]
+    def __len__(self): return len(self.h)
+class MaxHeap(MinHeap):
+    def heappush(self, x): heapq.heappush(self.h, MaxHeapObj(x))
+    def heappop(self): return heapq.heappop(self.h).val
+    def __getitem__(self, i): return self.h[i].val
+class Solution():
+    def mergeHeaps(self, a, b, n, m):
+        #your code here
+        return sorted(a+b,reverse=True)
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def isMerged(arr1, arr2, merged):
+    if (len(arr1) + len(arr2) != len(merged)):
+        return False
+    arr1 += arr2
+    arr1.sort()
+    mergedCopy = sorted(merged)
+    if (arr1 != mergedCopy):
+        return False
+    for i in range(1, len(merged)):
+        if merged[i] > merged[(i-1)//2]:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n, m = map(int, input().split())
+        a = [int(i) for i in input().split()]
+        b = [int(i) for i in input().split()]
+        copyA = a[:]
+        copyB = b[:]
+        obj = Solution()
+        merged = obj.mergeHeaps(a, b, n, m)
+        flag = isMerged(copyA, copyB, merged)
+        print(0 if flag == False else 1)
+
+# } Driver Code Ends
