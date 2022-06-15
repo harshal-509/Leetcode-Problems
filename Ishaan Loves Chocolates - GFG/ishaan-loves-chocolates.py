4972917bb72cf3e64d5af7608af0b1acea79f5c5
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-06-15 19:06:21 +0530

    Added solution - LeetHub

---
+++ Ishaan Loves Chocolates - GFG/ishaan-loves-chocolates.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+def chocolates (arr, n) : 
+    #Complete the function
+    return min(arr)
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    ans = chocolates(arr, n)
+    print(ans)
+    
+
+
+
+
+
+# } Driver Code Ends
