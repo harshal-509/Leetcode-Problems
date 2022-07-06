f55985eb2a42d35c3503f7ec90cf90c9fbc81017
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2022-07-06 12:39:29 +0530

    Time: 1170 ms (12.54%), Space: 13.8 MB (54.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def solve(n):
+            if(n==0):
+                return 0
+            if(n==1):
+                return 1
+            return solve(n-1)+solve(n-2)
+        return solve(n)
