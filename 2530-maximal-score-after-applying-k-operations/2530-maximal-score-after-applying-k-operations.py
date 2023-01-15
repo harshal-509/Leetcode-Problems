8333def99e14489788c0640d09b58a2de8de76c9
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2023-01-15 11:07:43 +0530

    Time: 3396 ms (8.44%), Space: 41.7 MB (6.03%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -0,0 +1,27 @@
+class MaxHeapObj:
+    def __init__(self,val): self.val = val
+    def __lt__(self,other): return self.val > other.val
+    def __eq__(self,other): return self.val == other.val
+    def __str__(self): return str(self.val)
+class MinHeap(object):
+    def __init__(self): self.h = []
+    def heappush(self, x): heapq.heappush(self.h, x)
+    def heappop(self): return heapq.heappop(self.h)
+    def __getitem__(self, i): return self.h[i]
+    def __len__(self): return len(self.h)
+class MaxHeap(MinHeap):
+    def heappush(self, x): heapq.heappush(self.h, MaxHeapObj(x))
+    def heappop(self): return heapq.heappop(self.h).val
+    def __getitem__(self, i): return self.h[i].val
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        ans=0
+        m=MaxHeap()
+        for i in nums:
+            m.heappush(i)
+        while(k):
+            p=m.heappop()
+            ans+=p
+            m.heappush(int(math.ceil(p/3)))
+            k-=1
+        return ans
