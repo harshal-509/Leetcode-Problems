67901f357d4bc6db989de1421fa1533872c76fbb
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2023-01-20 01:20:09 +0530

    Time: 317 ms (69.66%), Space: 18.9 MB (21.05%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remainderFrq = defaultdict(int)
+        remainderFrq[0] = 1
+        res = prefixSum = 0
+        for n in nums:
+            prefixSum += n
+            remainder = prefixSum % k
+            res += remainderFrq[remainder]
+            remainderFrq[remainder] += 1
+        return res
