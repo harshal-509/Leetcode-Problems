5a9d353ae6c1eb6533c2c031025f6b79e8af5f9a
Harshal Sharma <60326183+harshal-509@users.noreply.github.com>
2023-03-31 11:18:38 +0530

    Time: 218 ms (68.84%), Space: 16 MB (29.13%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    MOD = 10**9 + 7
+    def ways(self, pizza: List[str], k: int) -> int:
+        @cache
+        def solve(i, j, p):
+            if p == 1:
+                return 1 if apple[i][j] > 0 else 0
+            ans = 0
+            for r in range(i+1, m):
+                if apple[i][j] - apple[r][j] > 0:
+                    ans = (ans + solve(r, j, p-1)) % self.MOD
+            for c in range(j+1, n):
+                if apple[i][j] - apple[i][c] > 0:
+                    ans = (ans + solve(i, c, p-1)) % self.MOD
+            return ans
+        m, n = len(pizza), len(pizza[0])
+        apple = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                apple[i][j] = apple[i+1][j] + apple[i][j+1] - apple[i+1][j+1] + (pizza[i][j] == 'A')
+        return solve(0, 0, k)
